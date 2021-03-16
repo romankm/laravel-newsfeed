57e9071b112fd76f771c004e290adf0b78405f44
romank <romank@gmail.com>
2021-03-16 15:07:06 +0200

    added Category and NewsItem models

---
+++ resources/views/main.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>News</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
+</head>
+<body>
+
+<div class="d-flex align-items-center justify-content-center">
+    <h1> Main page </h1>
+</div>
+
+<div class="container d-flex align-items-center justify-content-center">
+    <div>
+        <div class="card" style="width: 50rem;">
+            <div class="card-body">
+                <h5 class="card-title">Titi asfas Titi asfas Titi asfas Titi asfas </h5>
+                <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+                <p class="card-text">content.</p>
+            </div>
+        </div>
+    </div>
+</div>
+</body>
+</html>
